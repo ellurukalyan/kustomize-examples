3054a4c621dc9752acbcb6e23d0daff35038bb3d
ellurukalyan <ellurukalyan.com>
2023-01-16 10:30:24 +0000

    new files

---
+++ envs-namespace/overlays/dev/schatgpt.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+curl https://api.openai.com/v1/completions 2>/dev/null \
+  -H "Content-Type: application/json" \
+  -H "Authorization: Bearer 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" \
+  -d '{
+  "model": "code-davinci-002",
+  "prompt": "<!-- Create a web page with the title sachin tendulkar -->\n<!DOCTYPE html>",
+  "temperature": 0,
+  "max_tokens": 1256,
+  "top_p": 1,
+  "frequency_penalty": 0,
+  "presence_penalty": 0
+}' \
+--insecure | jq '.choices[]'.text > index.html
