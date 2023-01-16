9cf8ff4ae2722384f7dbdf0ce41a139651dc46f7
ellurukalyan <ellurukalyan@gmail.com>
2023-01-16 17:52:53 +0530

    Update schatgpt.sh

--- envs-namespace/overlays/dev/schatgpt.sh
+++ envs-namespace/overlays/dev/schatgpt.sh
@@ -2,7 +2,7 @@
 
 curl https://api.openai.com/v1/completions 2>/dev/null \
   -H "Content-Type: application/json" \
-  -H "Authorization: Bearer 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" \
+  -H "Authorization: Bearer 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" \
   -d '{
   "model": "code-davinci-002",
   "prompt": "<!-- Create a web page to create Vm for Azure -->\n<!DOCTYPE html>",
