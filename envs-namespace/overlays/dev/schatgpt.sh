628cee861b685871476d1489742dfbf73943ba1f
ellurukalyan <ellurukalyan@gmail.com>
2023-01-17 17:09:50 +0530

    Update schatgpt.sh

--- envs-namespace/overlays/dev/schatgpt.sh
+++ envs-namespace/overlays/dev/schatgpt.sh
@@ -1,15 +1,22 @@
 #!/bin/sh
 
+echo '$1: API Tocken: '$1
+echo '$2: Enter your Question: '$2
+
+echo "\nInput": $1
+echo "\nOutput:" 
+
+curl=`cat <<EOS
 curl https://api.openai.com/v1/completions 2>/dev/null \
-  -H "Content-Type: application/json" \
-  -H "Authorization: Bearer " \
+  -H 'Content-Type: application/json' \
+  -H "Authorization: Bearer $1" \
   -d '{
-  "model": "code-davinci-002",
-  "prompt": "<!-- Create a web page to create Vm for Azure -->\n<!DOCTYPE html>",
-  "temperature": 0,
-  "max_tokens": 1256,
-  "top_p": 1,
-  "frequency_penalty": 0,
-  "presence_penalty": 0
+  "model": "text-davinci-003",
+  "prompt": "<!-- $2 -->\n<!DOCTYPE html>",
+  "max_tokens": 4000
+
 }' \
 --insecure | jq '.choices[]'.text > index.html
+EOS`
+
+eval ${curl}
